aa2ceb06aa5077b5ae2001732366937349a79f87
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-04 02:44:06 +0530

    So the feedback from the gemini ai and the correct answer for the question will be shown here in th feedback page have done succesffully

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+"use client";
+import { db } from "@/utils/db";
+import { UserAnswer } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import React, { useEffect, useState } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ChevronsUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
-const Feedback = () => {
+const Feedback = ({ params }) => {
+  const router = useRouter();
+  const [feedbackList, setFeedbackList] = useState([]);
+  const GetFeedback = async () => {
+    try {
+      const result = await db
+        .select()
+        .from(UserAnswer)
+        .where(eq(UserAnswer.mockIdRef, params.interviewId))
+        .orderBy(UserAnswer.id);
+
+      console.log("Result", result);
+      setFeedbackList(result);
+    } catch (error) {
+      console.error("Error fetching feedback:", error);
+    }
+  };
+  useEffect(() => {
+    GetFeedback();
+  }, [params.interviewId]);
   return (
-    <div>Feedback</div>
-  )
-}
+    <div className="p-10">
+      <h2 className="text-3xl font-bold text-green-500"> Congratulations</h2>
+      <h2 className="font-bold text-2xl">Here is your Interview Feedback</h2>
+      <h2 className="text-primary text-lg my-3">
+        Your Overall Interview Rating <strong>1/10</strong>
+      </h2>
 
-export default Feedback
+      <h2>
+        Find below interview questions with correct answer, Your answer and
+        feedback for improvement
+      </h2>
+      {feedbackList &&
+        feedbackList.map((item, index) => (
+          <Collapsible key={index} className="mt-7">
+            <CollapsibleTrigger className="p-2 bg-secondary rounded-lg flex justify-between my-2 text-left gap-7 w-full">
+              {item.question} <ChevronsUpDown className="h-4 w-4" />
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="flex flex-col gap-2">
+                <h2 className="text-red-500 p-2 border rounded-lg">
+                  <strong>Rating :</strong>
+                  {item.rating}
+                </h2>
+                <h2 className="p-2 border rounded-lg bg-red-50 text-sm text-red-900">
+                  <strong>Your Answer : </strong> {item.userAns}
+                </h2>
+                <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-900">
+                  <strong>Correct Answer : </strong> {item.correctAns}
+                </h2>
+                <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-blue-900">
+                  <strong>Feedback : </strong> {item.Feedback}
+                </h2>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
+      <Button onClick={() => router.replace("/dashboard")}>Go Home</Button>
+    </div>
+  );
+};
+
+export default Feedback;
