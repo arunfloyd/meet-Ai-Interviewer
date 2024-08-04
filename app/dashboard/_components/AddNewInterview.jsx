c1712a851d55739a4c5389cfe2b96b986ec48ef5
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-04 20:34:58 +0530

    Merge branch the main page or the starting page

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -158,6 +158,7 @@ const AddNewInterview = () => {
                       </div>
                     ) : (
                       "Start Interview"
+                      
                     )}
                   </Button>
                 </div>
